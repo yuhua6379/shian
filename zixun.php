eb0bec1f92e65c2b10e1b39c5ac0478327ee67b5
yuhua <yuhua.yh@alibaba-inc.com>
2018-07-25 15:05:13 +0800

    增加咨询后台逻辑

--- zixun.php
+++ zixun.php
@@ -1,12 +1,15 @@
 <?php
 
-	require("common.php");
+	// require("common.php");
 
 
 	$file  = '/serverlog/log'.date("Y-m-d").'.txt';//要写入文件的文件名（可以是任意文件名），如果文件不存在，将会创建一个
 		$content = "[error]"." ".date('[y-m-d h:i:s'],time())." "."adadasd00000000000"."\n";  
+		$f= 'SD';
 		if($f  = file_put_contents($file, $content,FILE_APPEND)){// 这个函数支持版本(PHP 5) 
 			echo "写入成功。<br />";
+		}else{
+			echo $f;
 		}
 
 	// $file  = 'log'.date("Y-m-d").'.txt';//要写入文件的文件名（可以是任意文件名），如果文件不存在，将会创建一个
@@ -15,16 +18,16 @@
 	// 	echo "写入成功。<br />";
 	// }
 
-	$name = $_POST["name"];
-	$phone = $_POST["phone"];
+	// $name = $_POST["name"];
+	// $phone = $_POST["phone"];
 
-	$yuchan_year = $_POST["year"];
-	$yuchan_month = $_POST["month"];
-	$yuchan_day = $_POST["day"];
+	// $yuchan_year = $_POST["year"];
+	// $yuchan_month = $_POST["month"];
+	// $yuchan_day = $_POST["day"];
 
-	$zixun_desc = $_POST["zixun_desc"];
+	// $zixun_desc = $_POST["zixun_desc"];
 
-	debug($name." ".$phone);
+	// debug($name." ".$phone);
 
 
 ?>
